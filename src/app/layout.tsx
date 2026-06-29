40f9e1c393f8ba19d13c452da9fae7cd370d5747
strovayapi <strovayapi@gmail.com>
2026-06-29 06:04:25 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
     >
       <head>
         {/* İkon kodunu buraya ekleyin */}
-        <link rel="icon" href="/ico.jpg" type="image/jpg" />
+        <link rel="icon" href="/favicon.ico" type="image/ico" />
       </head>
       <body className="min-h-full flex flex-col bg-black text-[#f0f0f0]">
         <Navbar />
