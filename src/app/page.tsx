68e1f4b823c92ccb59a9950447adbc7ee1bf73c3
strovayapi <strovayapi@gmail.com>
2026-06-29 08:32:16 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Ana Sayfa",
+  title: "Strova İnşaat",
 };
 
 export default function HomePage() {
