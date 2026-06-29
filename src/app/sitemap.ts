f803c31d59fd5c285fa50b6603deb724c806dd2b
strovayapi <strovayapi@gmail.com>
2026-06-29 07:39:54 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,22 +3,34 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://strovainsaat.com',
+      url: 'https://strovainsaat.com', // Anasayfa
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://strovainsaat.com/projelerimiz', // Sayfalarınızı buraya ekleyin
+      url: 'https://strovainsaat.com/hakkimizda', // Hakkımızda
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://strovainsaat.com/iletisim',
+      url: 'https://strovainsaat.com/hizmetler', // Hizmetler
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://strovainsaat.com/projeler', // Projeler
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://strovainsaat.com/iletisim', // İletişim
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.5,
+      priority: 0.7,
     },
   ];
 }
